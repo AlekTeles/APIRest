fa8c7c988536ad3925814c6140b33c43a026f8aa
AlekTeles <86433407+AlekTeles@users.noreply.github.com>
2023-09-18 15:04:43 -0300

    Fastify Plugins

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
-console.log(process.env)
 
 export const config: Knex.Config = {
   client: 'sqlite',
