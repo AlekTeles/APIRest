e7831d87ae120fa843a866a2d5c127e25574971f
AlekTeles <86433407+AlekTeles@users.noreply.github.com>
2023-09-18 17:22:02 -0300

    Transactions Creation

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,14 +1,26 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { knex } from '../database'
+import crypto from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-      .returning('*')
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
 
-    return transaction
+    const transaction = await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
